9f3c535bfcceae36221c40f9fb84d27c04c89d03
Patrick Evers <patrickevers2@web.de>
2019-11-26 13:26:23 +0100

    Funktion um das für die Entschlüsselung gesuchte Passwort zu finden.

--- home.js
+++ home.js
@@ -2,13 +2,22 @@ const fs = require('fs');
 const crypto = require('crypto');
 const fsasync = fs.promises;
 
-document.getElementById('submitBtn').addEventListener('click', () =>{
+document.getElementById('encBtn').addEventListener('click', () =>{
     var usedFor = document.getElementById('used-for').value;
     var password = document.getElementById('password').value;
     var masterPassword = document.getElementById('master-password').value;
+
     var content = usedFor + ": " + enc(masterPassword, password) + "\n";
     fsasync.appendFile('passwords.txt', content, 'utf8'); 
-    console.log(usedFor+": "+password+"\n"+masterPassword);
+})
+
+document.getElementById('decBtn').addEventListener('click', () =>{
+    var usedFor = document.getElementById('used-for').value;
+    var masterPassword = document.getElementById('master-password').value;
+    var allPasswords = fs.readFileSync('passwords.txt','utf8').toString();
+    var password = allPasswords.substring(allPasswords.indexOf(usedFor+": "));
+    password = password.substring(0,password.indexOf("\n"));
+    
 })
 
 
