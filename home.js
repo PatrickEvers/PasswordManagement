7aabd161c0773f1ca92bcc6f8e848b56d01cd93f
Patrick Evers <patrickevers2@web.de>
2019-11-26 12:35:31 +0100

    Speichern von verschlüsselten Passwörtern hinzugefügt

--- home.js
+++ home.js
@@ -1,3 +1,21 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const fsasync = fs.promises;
+
+document.getElementById('submitBtn').addEventListener('click', () =>{
+    var usedFor = document.getElementById('used-for').value;
+    var password = document.getElementById('password').value;
+    var masterPassword = document.getElementById('master-password').value;
+    var content = usedFor + ": " + enc(masterPassword, password) + "\n";
+    fsasync.appendFile('passwords.txt', content, 'utf8'); 
+    console.log(usedFor+": "+password+"\n"+masterPassword);
+})
+
+
+function enc (masterPassword, password){
+    var key = crypto.createCipher('aes-128-cbc', masterPassword);
+    var str = key.update(password, 'utf8', 'hex')
+    str += key.final('hex');
+    
+    return(str);
+}
