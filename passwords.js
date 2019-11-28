cb8c132dfae2a1c9a6d2acbd73706cb241930bed
Patrick Evers <patrickevers2@web.de>
2019-11-28 10:55:39 +0100

    Klickevent für die Buttons hinzugefügt, um das Passwort anzuzeigen

--- passwords.js
+++ passwords.js
@@ -13,16 +13,34 @@ for(var i=0;i<allPasswords.length;i++){
         var tr = document.createElement('tr')
         var td1 = document.createElement('td');
         var td2 = document.createElement('td');
-        var input = document.createElement("input");
+        var td3 = document.createElement('td');
+        var input = document.createElement('input');
+        var button = document.createElement('button');
 
         td1.textContent = allPasswords[i][0];
         input.value = allPasswords[i][1];
         input.type = 'password';
         input.readOnly = true;
+        input.id = 'pw'+(i+1);
+        button.textContent = 'Passwort anzeigen';
+        button.id = (i+1);
+        button.addEventListener('click', showPassword)
 
         document.getElementById('main-table').appendChild(tr);
         tr.appendChild(td1);
         tr.appendChild(td2);
+        tr.appendChild(td3);
         td2.appendChild(input);
+        td3.appendChild(button);
+    }
+}
+
+function showPassword(event){
+    var input = document.getElementById('pw'+event.target.id);
+    if(input.type == 'password'){        
+        input.type = 'text';
+    }
+    else{
+        input.type = 'password';
     }
 }
