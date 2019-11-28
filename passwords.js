16c40005c90e5dd84306cbc14892cb931ae17759
Patrick Evers <patrickevers2@web.de>
2019-11-28 10:06:44 +0100

    Passwörter werden in einer Tabelle angezeigt

--- passwords.js
+++ passwords.js
@@ -1,8 +1,28 @@
 const fs = require('fs');
 
 var allPasswords = fs.readFileSync('passwords.txt','utf8').toString().split('\n');
+
+//Aufteilung von Verwendungszweck & Passwort
 for(var i=0;i<allPasswords.length;i++){
-    var li = document.createElement('li');
-    li.textContent = allPasswords[i];
-    document.getElementById('main-list').appendChild(li);
+    allPasswords[i] = allPasswords[i].split(' ');
+}
+
+//Schreibe Passwörter in die Tabelle
+for(var i=0;i<allPasswords.length;i++){
+    if(allPasswords[i] != ""){
+        var tr = document.createElement('tr')
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var input = document.createElement("input");
+
+        td1.textContent = allPasswords[i][0];
+        input.value = allPasswords[i][1];
+        input.type = 'password';
+        input.readOnly = true;
+
+        document.getElementById('main-table').appendChild(tr);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        td2.appendChild(input);
+    }
 }
