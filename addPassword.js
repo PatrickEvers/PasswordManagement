a53e0f02ce602a36ab875c614cdba11786e00b3d
Patrick Evers <patrickevers2@web.de>
2019-11-28 13:30:22 +0100

    Funktionen zum Entschlüsseln & Anzeigen das Passworts entfernt

--- addPassword.js
+++ addPassword.js
@@ -12,31 +12,6 @@ document.getElementById('encBtn').addEventListener('click', () =>{
     fsasync.appendFile('passwords.txt', content, 'utf8');
 })
 
-//Klickevent für das Entschlüsseln
-document.getElementById('decBtn').addEventListener('click', () =>{
-    var usedFor = document.getElementById('used-for').value;
-    var masterPassword = document.getElementById('master-password').value;    
-    var allPasswords = fs.readFileSync('passwords.txt','utf8').toString();
-
-   //Hole Passwort anhand des Verwendungszwecks aus der Datei.    
-    var password = allPasswords.substring(allPasswords.indexOf(usedFor+": "));
-    password = password.substring(0,password.indexOf("\n"));
-    password = password.substring(password.indexOf(" ")+1).toString();
-    
-    document.getElementById('password').value = decrypt(password, masterPassword);
-})
-
-//Klickevent für das Anzeigen des Passworts
-showPwBtn.addEventListener('click', () =>{
-    var password = document.getElementById('password');
-    if(password.type == 'password'){        
-        password.type = 'text';
-    }
-    else{
-        password.type = 'password';
-    }
-})
-
 MPwBtn.addEventListener('click', () =>{
     var form = document.getElementById("myForm");
     if (form.style.display === "none") {
@@ -53,12 +28,4 @@ function encrypt (masterPassword, password){
     str += key.final('hex');
     
     return(str);
-}
-
-//Funktion für die Entschlüsselung
-function decrypt(string, key) {
-    var decipher = crypto.createDecipher('aes-128-cbc', key)
-    var dec = decipher.update(string, 'hex', 'utf8')
-    dec += decipher.final('utf8')
-    return dec;
 }
